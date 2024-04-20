bc5436db66c318083abfabecbdc8ba877773398b
ashton :3 <122162553+im-ashton@users.noreply.github.com>
2024-04-20 15:16:34 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,14 +35,14 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
+          I'm the founder of{" "}
           <Link
             target="_blank"
-            href="https://unkey.dev"
+            href="https://github.com"
             className="underline duration-500 hover:text-zinc-300"
           >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+            bloom labs.
+          </Link> We are trying to make tech somehow useful and fun.
         </h2>
       </div>
     </div>
