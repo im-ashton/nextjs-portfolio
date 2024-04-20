71b9c0e38554ac7ca21235abfc24d32b8fe57655
ashton :3 <122162553+im-ashton@users.noreply.github.com>
2024-04-20 15:23:17 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "ashton's work",
+    template: "%s | ashton's work",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
